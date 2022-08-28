c84a704ac7d55da78eec3431069d5d2cbd47304b
GERJanB <jan.budde.jb@gmail.com>
2022-08-28 15:04:25 +0200

    export

--- src/main/java/com/gamereferee/MainApplication.java
+++ src/main/java/com/gamereferee/MainApplication.java
@@ -12,7 +12,7 @@ public class MainApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 650, 800);
-        stage.setTitle("Hello!");
+        stage.setTitle("Mühle Referee");
         stage.setScene(scene);
         stage.show();
     }
