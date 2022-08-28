ef1c6aaff2e4207f9b50dc1269cd7ec9ea3d0ef4
GERJanB <jan.budde.jb@gmail.com>
2022-08-28 10:25:12 +0200

    remove examples

--- src/main/java/com/example/gamereferee/MainController.java
+++ src/main/java/com/example/gamereferee/MainController.java
@@ -5,18 +5,5 @@ import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-    @FXML
-    protected void Test() {
-        welcomeText.setText("Hello World");
-        Color color1 = Color.rgb(23,23,23);
-        welcomeText.setTextFill(color1);
-    }
 }
