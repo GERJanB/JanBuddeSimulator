d37a7ca4988d2a6ea128ba1083f1e8175857e879
GERJanB <jan.budde.jb@gmail.com>
2022-08-30 13:04:02 +0200

    create Move

--- src/main/java/Model/Move.java
+++ src/main/java/Model/Move.java
@@ -1,5 +1,34 @@
 package Model;
 
-public class Move {
+import javafx.scene.layout.Region;
 
+public class Move {
+    private int fromRow;
+    private int fromCol;
+
+    private int toRow;
+    private int toCol;
+
+    public Move(int fromRow, int fromCol, int toRow, int toCol) {
+        this.fromRow = fromRow;
+        this.fromCol = fromCol;
+        this.toRow = toRow;
+        this.toCol = toCol;
+    }
+
+    public int getFromRow() {
+        return fromRow;
+    }
+
+    public int getFromCol() {
+        return fromCol;
+    }
+
+    public int getToRow() {
+        return toRow;
+    }
+
+    public int getToCol() {
+        return toCol;
+    }
 }
