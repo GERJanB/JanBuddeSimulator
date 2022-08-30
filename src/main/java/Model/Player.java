fae56689511b99cc5dc66e86f9fe2d3f7a5d7935
GERJanB <jan.budde.jb@gmail.com>
2022-08-30 13:03:46 +0200

    create Player

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,5 +1,21 @@
 package Model;
 
 public class Player {
-    
+    private Boolean isHumanPlayer;
+    private Boolean isPlayerA;
+
+    private Board board;
+
+    public Player(Boolean isHumanPlayer, Boolean isPlayerA) {
+        this.isHumanPlayer = isHumanPlayer;
+        this.isPlayerA = isPlayerA;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    public void setBoard(Board board) {
+        this.board = board;
+    }
 }
