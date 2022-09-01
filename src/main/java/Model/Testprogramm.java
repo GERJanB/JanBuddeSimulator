f462e91539315c895fafb32a5f70ef0c11d00a6e
Jan Budde <jan.budde@amcon.de>
2022-09-01 15:08:25 +0200

    make board square

--- src/main/java/Model/Testprogramm.java
+++ src/main/java/Model/Testprogramm.java
@@ -8,18 +8,18 @@ public class Testprogramm {
         Referee referee = new Referee(playerA, playerB);
 
         Board board = new Board();
-        printBoard(board);
+        printBoard(referee.getBoard());
     }
 
     public static void printBoard(Board board) {
-        String dash11 = "-----------";
-        String dash7 = "-------";
-        String dash3 = "---";
-        String space11 = "           ";
-        String space7 = "       ";
-        String space3 = "   ";
-        String startSpace = "|   ";
-        String endSpace = "   |";
+        String dash14 = "--------------";
+        String dash9 = "---------";
+        String dash4 = "----";
+        String space14 = "              ";
+        String space9 = "         ";
+        String space4 = "    ";
+        String startSpace = "|    ";
+        String endSpace = "    |";
 
         Field[] outerFields = board.getOuterRing().getFields();
         Field[] secondFields = board.getSecondRing().getFields();
@@ -29,89 +29,89 @@ public class Testprogramm {
         for (int i = 0; i < 3; i++) {
             IO.print(printIcon(outerFields[i].getPiece()));
             if (i < 2) {
-                IO.print(dash11);
+                IO.print(dash14);
             }
         }
         IO.println();
 
         //print second Line
-        IO.println("|" + space11 + "|" + space11 + "|");
+        IO.println("|" + space14 + "|" + space14 + "|");
 
         //print second top
         IO.print(startSpace);
         for (int i = 0; i < 3; i++) {
             IO.print(printIcon(secondFields[i].getPiece()));
             if (i < 2) {
-                IO.print(dash7);
+                IO.print(dash9);
             }
         }
         IO.println(endSpace);
 
         //print fourth Line
-        IO.println("|" + space3 + "|" + space7 + "|" + space7 + "|" + space3 + "|");
+        IO.println("|" + space4 + "|" + space9 + "|" + space9 + "|" + space4 + "|");
 
         //print inner top
         IO.print(startSpace + startSpace);
         for (int i = 0; i < 3; i++) {
             IO.print(printIcon(innerFields[i].getPiece()));
             if (i < 2) {
-                IO.print(dash3);
+                IO.print(dash4);
             }
         }
         IO.print(endSpace);
         IO.println(endSpace);
 
         //print sixth Line
-        IO.println(startSpace + startSpace + "|" + space7 + "|" + endSpace + endSpace);
+        IO.println(startSpace + startSpace + "|" + space9 + "|" + endSpace + endSpace);
 
         //print center Line
         IO.print(printIcon(outerFields[7].getPiece()));
-        IO.print(dash3);
+        IO.print(dash4);
         IO.print(printIcon(secondFields[7].getPiece()));
-        IO.print(dash3);
+        IO.print(dash4);
         IO.print(printIcon(innerFields[7].getPiece()));
-        IO.print(space7);
+        IO.print(space9);
         IO.print(printIcon(innerFields[3].getPiece()));
-        IO.print(dash3);
+        IO.print(dash4);
         IO.print(printIcon(secondFields[3].getPiece()));
-        IO.print(dash3);
+        IO.print(dash4);
         IO.println(printIcon(outerFields[3].getPiece()));
 
         //print eighth Line
-        IO.println(startSpace + startSpace + "|" + space7 + "|" + endSpace + endSpace);
+        IO.println(startSpace + startSpace + "|" + space9 + "|" + endSpace + endSpace);
 
         //print inner bottom
         IO.print(startSpace + startSpace);
         for (int i = 6; i > 3; i--) {
             IO.print(printIcon(innerFields[i].getPiece()));
             if (i > 4) {
-                IO.print(dash3);
+                IO.print(dash4);
             }
         }
         IO.print(endSpace);
         IO.println(endSpace);
 
         //print second Spacer
-        IO.println("|" + space3 + "|" + space7 + "|" + space7 + "|" + space3 + "|");
+        IO.println("|" + space4 + "|" + space9 + "|" + space9 + "|" + space4 + "|");
 
         //print second bottom
         IO.print(startSpace);
         for (int i = 6; i > 3; i--) {
             IO.print(printIcon(secondFields[i].getPiece()));
             if (i > 4) {
-                IO.print(dash7);
+                IO.print(dash9);
             }
         }
         IO.println(endSpace);
 
         //print bottom spacer
-        IO.println("|" + space11 + "|" + space11 + "|");
+        IO.println("|" + space14 + "|" + space14 + "|");
 
         //print outer bottom
         for (int i = 6; i > 3; i--) {
             IO.print(printIcon(outerFields[i].getPiece()));
             if (i > 4) {
-                IO.print(dash11);
+                IO.print(dash14);
             }
         }
         IO.println();
