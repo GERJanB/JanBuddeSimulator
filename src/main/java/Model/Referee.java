c730aab3f3e30c1880c1b7a28c247ef408e04885
GERJanB <jan.budde.jb@gmail.com>
2022-10-26 09:03:34 +0200

    simplify mill check

--- src/main/java/Model/Referee.java
+++ src/main/java/Model/Referee.java
@@ -43,7 +43,7 @@ public class Referee {
                 moveOuter.setToRing(1);
                 moveOuter.setToPosition(i);
 
-                if (GetMill(moveOuter) == null) return false;
+                return GetMill(moveOuter);
             }
 
             if (secondFields[i].getPiece() != null) {
@@ -51,7 +51,7 @@ public class Referee {
                 moveSecond.setToRing(2);
                 moveSecond.setToPosition(i);
 
-                if (GetMill(moveSecond) == null) return false;
+                return GetMill(moveSecond);
             }
 
             if (innerFields[i].getPiece() != null) {
@@ -59,110 +59,52 @@ public class Referee {
                 moveInner.setToRing(3);
                 moveInner.setToPosition(i);
 
-                if (GetMill(moveInner) == null) return false;
+                return GetMill(moveInner);
             }
         }
         return true;
     }
 
-    public Boolean MoveIsMill(Move move) {
-        if (currentPlayer.getPiecesCountBoard() < 3) {
-            return false;
-        }
-        Mill mill = GetMill(move);
-
-        if (mill == null) {
-            return false;
-        }
-
-        if (!mill.isMultiRing()) {
-            for (int i = 0; i < 3; i++) {
-                currentPlayer.getBoard().getRing(mill.getRings()[0]).getFields()[mill.getPositions()[i]].getPiece().setInMill(mill.isMill());
-            }
-        } else {
-            for (int i = 0; i < 3; i++) {
-                currentPlayer.getBoard().getRing(mill.getRings()[i]).getFields()[mill.getPositions()[0]].getPiece().setInMill(mill.isMill());
-            }
-        }
-        return mill.isMill();
-    }
-
-    public Mill GetMill(Move move) {
+    public boolean GetMill(Move move) {
         int position = move.getToPosition();
 
-        int[] rings = {-1,-1,-1};
-        int[] positions = {-1,-1,-1};
-
+        //check on same Ring
+        Field[] fields = currentPlayer.getBoard().getRing(move.getToRing()).getFields();
         if (position % 2 == 0) {
-            int pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-
-            switch (position) {
-                case 0: pos1 = 1; pos2 = 2; pos3 = 7; pos4 = 6; break;
-                case 2: pos1 = 3; pos2 = 4; pos3 = 1; pos4 = 0; break;
-                case 4: pos1 = 5; pos2 = 6; pos3 = 3; pos4 = 2; break;
-                case 6: pos1 = 7; pos2 = 0; pos3 = 5; pos4 = 4; break;
+            if (((fields[((position + 1) % 8)] != null && fields[((position + 2) % 8)] != null)
+                    && (fields[((position + 1) % 8)].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
+                    && fields[((position + 2) % 8)].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()))
+                || ((fields[((position - 1) % 8)] != null && fields[((position - 2) % 8)] != null)
+                    && (fields[((position - 1) % 8)].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
+                    && fields[((position - 2) % 8)].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()))) {
+                return true;
             }
-
-            Field[] fields = currentPlayer.getBoard().getRing(move.getToRing()).getFields();
-            if ((fields[pos3].getPiece() != null && fields[pos4].getPiece() != null)
-                    &&(fields[pos3].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
-                    && fields[pos4].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA())) {
-                rings[0] = move.getToRing();
-                positions[0] = pos3;
-                positions[1] = pos4;
-                positions[2] = position;
-                return new Mill(rings, positions, true, false);
-            }
-
-            if ((fields[pos1].getPiece() != null && fields[pos2].getPiece() != null)
-                    && (fields[pos1].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
-                    && fields[pos2].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA())){
-                rings[0] = move.getToRing();
-                positions[0] = pos1;
-                positions[1] = pos2;
-                positions[2] = position;
-                return new Mill(rings, positions, true, false);
-            }
-
         } else {
-            //check on same Ring
-            int pos1 = position -1;
-            int pos2 = position == 7 ? 0 : position + 1;
-
-            Field[] fields = currentPlayer.getBoard().getRing(move.getToRing()).getFields();
-            if ((fields[pos1].getPiece() != null && fields[pos2].getPiece() != null)
-                    && (fields[pos1].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
-                    && fields[pos2].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA())) {
-                rings[0] = move.getToRing();
-                positions[0] = pos1;
-                positions[1] = pos2;
-                positions[2] = position;
-                return new Mill(rings, positions, true, false);
-            }
-
-            //Check for Mills overlapping Rings
-            Field[] outerFields = currentPlayer.getBoard().getRing(1).getFields();
-            Field[] secondFields = currentPlayer.getBoard().getRing(2).getFields();
-            Field[] innerFields = currentPlayer.getBoard().getRing(3).getFields();
-
-            for (int i = 0; i < 8; i++) {
-                if (i % 2 == 0) {
-                    continue;
-                }
-                if ((outerFields[i].getPiece() != null && secondFields[i].getPiece() != null && innerFields[i].getPiece() != null)
-                        && (outerFields[i].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
-                        && secondFields[i].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
-                        && innerFields[i].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA())) {
-                    rings[0] = 1;
-                    rings[1] = 2;
-                    rings[2] = 3;
-                    positions[0] = i;
-                    return new Mill(rings,positions, true, true);
-                }
+            if ((fields[((position + 1) % 8)] != null && fields[((position - 1) % 8)] != null)
+                    && (fields[((position + 1) % 8)].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
+                    && fields[((position - 1) % 8)].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA())) {
+                return true;
             }
         }
 
-        return null;
+        //Check for Mills overlapping Rings
+        Field[] outerFields = currentPlayer.getBoard().getRing(1).getFields();
+        Field[] secondFields = currentPlayer.getBoard().getRing(2).getFields();
+        Field[] innerFields = currentPlayer.getBoard().getRing(3).getFields();
+
+        for (int i = 0; i < 8; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            if ((outerFields[i].getPiece() != null && secondFields[i].getPiece() != null && innerFields[i].getPiece() != null)
+                    && (outerFields[i].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
+                    && secondFields[i].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA()
+                    && innerFields[i].getPiece().getBelongsPlayerA() == currentPlayer.isPlayerA())) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public Player getCurrentPlayer() {
